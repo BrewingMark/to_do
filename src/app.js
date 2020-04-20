07b05d12c42e67a888ed4ef565d620c046a26071
Mark Nicol <marknicol1991@live.co.uk>
2020-04-20 20:06:07 +0100

    MVP complete.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,21 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
-    el: "#app"
+    el: "#app",
+    data: {
+      tasks: [{name:"Buy Beer"},
+              {name:"Wash Car"},
+              {name: "Order Birthday Cake"}
+             ],
+      newTask: ""
+    },
+    methods: {
+      saveNewTask: function(){
+        this.tasks.push({
+          name: this.newTask
+        });
+        this.newTask = "";
+      }
+    }
   });
 });
